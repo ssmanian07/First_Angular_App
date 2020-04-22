234e78888acd5e12dfaf4ade7c7fa28b5e3353c4
ssmanian07 <ssmanianmca@gmail.com>
2020-04-22 18:38:12 +0530

    First Code base

---
+++ src/app/courses.component.ts
@@ -0,0 +1,20 @@
+import {Component } from '@angular/core'
+
+@Component({
+    selector : 'courses',
+    template : `
+    <h2>{{ title }}</h2>
+    
+    <ul>
+    <li *ngFor="let course of courses">
+    {{ course }}
+    </li>
+    </ul>
+    `
+})
+
+export class CoursesComponent {
+    title = 'List of Courses';
+    courses = ["Course 1", "Course 2", "Course 3"]
+}
+
