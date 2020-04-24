55303287012843375078f1bb002ffe05f3a35f85
ssmanian07 <ssmanianmca@gmail.com>
2020-04-24 17:35:38 +0530

    favorite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,30 @@ import { AuthorsService } from './authors.service';
 import { CourseService } from './courses.service';
 import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    FavoriteComponent,
+    TitleCasePipe    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     CourseService,
---
+++ src/app/title-case.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'titleCase',
+})
+export class TitleCasePipe implements PipeTransform {
+  transform(value: string): string {
+    if (!value) return null;
+
+    let words = value.split(' ');
+
+    console.log(words.length);
+
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+
+      if (i > 0 && this.isPreposition(word)) words[i] = word.toLowerCase();
+      else words[i] = this.toTitleCase(word);
+    }
+    return words.join(' ');
+  }
+  private toTitleCase(word: string): string {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+  }
+
+  private isPreposition(word: string): boolean {
+    let prepositoin = ['the', 'of'];
+
+    return prepositoin.includes(word.toLowerCase());
+  }
+}
